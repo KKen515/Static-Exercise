84f47aa6f430d406d69886591a7be008811b4b26
Kenton Knelle <kken51915@gmail.com>
2025-09-10 00:24:42 -0400

    Exercise 1 Push

---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,14 @@
+﻿namespace StaticExercise;
+
+public static class TempConverter
+{
+    public static double FahrenheitCelsius(double Fahrenheit)
+    {
+        return (Fahrenheit - 32) * 5 / 9;
+    }
+
+    public static double CelsiusToFahrenheit(double Celsius)
+    {
+        return (Celsius * 9 / 5) + 32;
+    }
+}
